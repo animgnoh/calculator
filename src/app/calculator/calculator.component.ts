49fb67d23f4b80e931b65a954410384dacee5d7c
Mina Hong <minahong@Minaui-MacBookPro.local>
2020-03-15 20:24:17 +0900

    Add some basic functions

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
+  display: string = '0';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  keyClicked(number: string) {
+    this.display += number;
+  }
+
+  reset() {
+    this.display = '';
   }
 
 }
